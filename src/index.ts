93f0cf769c8586007c18b9900e88d17afb54a3fd
Marie-France Fritz <mariefrance.fritz@outlook.fr>
2025-01-09 15:02:02 +0100

    fix bug with providers

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,31 @@ import bodyParser from "body-parser";
 import todoRoutes from "./routes/todoRoutes";
 
 const app = express();
+const cors = require("cors");
 const port = 3000;
 
-// Middleware
+// Autoriser toutes les origines (ou spécifie localhost:4200 uniquement)
+app.use(
+  cors({
+    origin: "http://localhost:4200", // Autoriser les requêtes de ce domaine
+    methods: ["GET", "POST", "PUT", "DELETE"], // Autoriser les méthodes HTTP nécessaires
+  })
+);
+
+// Middleware pour analyser le corps de la requête (JSON)
 app.use(bodyParser.json());
 
 // Routes
 app.use("/todos", todoRoutes);
 
-// 0. Hello World
+app.get("/todos", (req, res) => {
+  res.json([
+    { id: 1, text: "Learn Angular" },
+    { id: 2, text: "Build an app" },
+  ]);
+});
+
+// Hello World
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
 });
