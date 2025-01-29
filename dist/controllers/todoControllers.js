81b0181ccef1ac049d0058ab2786f2161e322db3
Marie-France Fritz <mariefrance.fritz@outlook.fr>
2025-01-29 09:26:04 +0100

    build in js

--- dist/controllers/todoControllers.js
+++ dist/controllers/todoControllers.js
@@ -3,21 +3,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteTodo = exports.updateTodo = exports.createTodo = exports.getTodoById = exports.getTodos = void 0;
 let todos = [];
 let nextId = 1;
+// Get all tasks
 const getTodos = (req, res) => {
     res.json(todos);
 };
 exports.getTodos = getTodos;
+// Get task by ID
 const getTodoById = (req, res) => {
     const id = parseInt(req.params.id);
     const todo = todos.find((task) => task.id === id);
-    if (todo) {
-        res.json(todo);
+    if (!todo) {
+        return res.status(404).json({ message: "Todo not found" });
     }
-    else {
-        res.status(404).json({ message: "Todo not found" });
-    }
+    res.json(todo);
 };
 exports.getTodoById = getTodoById;
+// Create task
 const createTodo = (req, res) => {
     const { title, description } = req.body;
     if (!title)
@@ -31,9 +32,10 @@ const createTodo = (req, res) => {
         completed: false,
     };
     todos.push(newTodo);
-    res.status(201).json({ message: "Task created !" });
+    res.status(201).json(newTodo);
 };
 exports.createTodo = createTodo;
+// Update task
 const updateTodo = (req, res) => {
     const id = parseInt(req.params.id);
     const { title, description, completed } = req.body;
@@ -46,15 +48,16 @@ const updateTodo = (req, res) => {
         todo.description = description;
     if (completed !== undefined)
         todo.completed = completed;
-    res.json({ message: "Task updated !" });
+    res.json({ message: "Task updated !", todo });
 };
 exports.updateTodo = updateTodo;
+// Delete task
 const deleteTodo = (req, res) => {
     const id = parseInt(req.params.id);
     const index = todos.findIndex((task) => task.id === id);
     if (index === -1)
         return res.status(404).json({ message: "Todo not found" });
     todos.splice(index, 1);
-    res.status(204).send().json({ message: "Task Deleted" });
+    res.status(204).send();
 };
 exports.deleteTodo = deleteTodo;
--- dist/index.js
+++ dist/index.js
@@ -5,17 +5,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
+const cors_1 = __importDefault(require("cors"));
 const todoRoutes_1 = __importDefault(require("./routes/todoRoutes"));
 const app = (0, express_1.default)();
-const cors = require("cors");
 const port = 3000;
 // Autoriser toutes les origines (ou spécifie localhost:4200 uniquement)
-app.use(cors({
+app.use((0, cors_1.default)({
     origin: "http://localhost:4200", // Autoriser les requêtes de ce domaine
     methods: ["GET", "POST", "PUT", "DELETE"], // Autoriser les méthodes HTTP nécessaires
 }));
 // Middleware pour analyser le corps de la requête (JSON)
 app.use(body_parser_1.default.json());
+// Route de test
+app.get("/", (req, res) => {
+    res.send("Welcome to the Todo API!");
+});
 // Routes
 app.use("/todos", todoRoutes_1.default);
 app.get("/todos", (req, res) => {
@@ -23,10 +27,6 @@ app.get("/todos", (req, res) => {
         { id: 1, text: "Learn Angular" },
         { id: 2, text: "Build an app" },
     ]);
-});
-// Hello World
-app.get("/", (req, res) => {
-    res.send("Hello World");
 });
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
